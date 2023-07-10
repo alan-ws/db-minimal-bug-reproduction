507e675934bffec5ce23d3d1fa23336648f22a01
Alan Williams <alan.williams@vercel.com>
2023-07-10 15:22:09 +0100

    init

--- apps/web/app/vercel/[slug]/page.tsx
+++ apps/web/app/vercel/[slug]/page.tsx
@@ -1,10 +1,13 @@
+import { notFound } from "next/navigation";
+
 export const dynamicParams = true;
 
 export function generateStaticParams() {
-  const slugs = [{slug: 'one'},{slug: 'two'},{slug: 'three'}]
+  const slugs = [{ slug: "one" }, { slug: "two" }, { slug: "three" }];
   return slugs;
 }
 
-export default function Page({ params }: any) {
+export default function Page({ params }: { params: { slug: string } }) {
+  notFound();
   return <h1>Slug: {params.slug}</h1>;
 }
